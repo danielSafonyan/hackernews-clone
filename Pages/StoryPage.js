9037eab7d351dbb359e5049966dca7e08da70e88
danielSafonyan <danielsafonyan@gmail.com>
2023-01-24 15:04:35 +0000

    chore: fetchStory -> pure function + add comment

--- Pages/StoryPage.js
+++ Pages/StoryPage.js
@@ -4,34 +4,30 @@ import Story from '../components/Story.js'
 import Comments from '../components/Comments.js'
 
 
-export default async function Item(path) {
-    let story = null;
-    let hasComments = false;  
-    let hasError = false;
+export default async function StoryPage(path) {
+    let story
+    let hasComments
     
     try { 
-     story = await fetchStory() 
-     hasComments = story.comments.length > 0;
-  } catch(error) {
-     hasError = true; 
-     console.error(error);
-  } 
-  
-  if (hasError) {
-     view.innerHTML = `<div class="error">Error fetching story</div>`;
-  }
-    
+        // get itemId from the URL hash
+        const itemId = window.location.hash.split('?id=').pop()
+        story = await fetchStory(itemId) 
+        hasComments = story.comments.length > 0;
+      } catch(error) {
+        view.innerHTML = `<div class="error">Error fetching story</div>`;
+        return
+      } 
+      
     view.innerHTML = `
-  <div>
-    ${Story(story)}
-  </div>
-  <hr/>
-  ${hasComments ? story.comments.map(comment => Comments(comment)).join('') : 'No comments'}
-  `  
+                    <div>
+                      ${Story(story)}
+                    </div>
+                    <hr/>
+                    ${hasComments ? story.comments.map(comment => Comments(comment)).join('') : 'No comments'}
+                `  
 }
 
-async function fetchStory() {
-    const itemId = window.location.hash.split('?id=').pop()
+async function fetchStory(itemId) {
     const resp = await fetch(`${baseUrl}/item/${itemId}`)
     const data = await resp.json()
     return data
